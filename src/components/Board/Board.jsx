a1907765f7a8ccc26eeacb5265ef810945cd91e5
Nicolas <nicobenpe@gmail.com>
2023-01-30 21:48:37 -0300

    Agregando detalles

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -4,13 +4,58 @@ import Square from "../Square/Square";
 const Board = () => {
   const [turn, setTurn] = useState('X');
   const [game, setGame] = useState(['','','','','','','','','']);
-
+  const [winner, setWinner] = useState ("Todavia no hay un ganador")
   
   const handleGame = (position)=>{
-    const gameArray = [... game];
-    gameArray [position] = 'X'
-    setGame(gameArray)
+    if(game [position] !== "") return;
+    const gameArray = [... game]; //! Para hacer copias de array
+    gameArray [position] = turn
+    setGame(gameArray);
+    if (turn === 'X'){
+      setTurn('O');
+    }else{
+      setTurn ('X')
+    }
+
   }
+
+let gameStatus;
+let flag = false;
+
+  const handleWinner = () => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    lines.forEach(([x, y, z]) => {
+
+      // console.log("1", game[x] == game[y]);
+      // console.log("2", game[y] == game[z]);
+      // console.log("3", game[x] != "");
+
+      if (game[x] == game[y] && game[y] == game[z] && game[x] != "") {
+        gameStatus = "GANO " + game[x];
+        flag = true;
+      } else {
+        if (!flag)
+          gameStatus = "Juegos " + turn;
+
+      }
+
+
+    });
+    console.log(gameStatus);
+  }
+
+handleWinner();
+
   return ( 
     <>
     <div>
@@ -28,8 +73,9 @@ const Board = () => {
       <Square symbol={game[7]} action={()=>handleGame(7)}/>
       <Square symbol={game[8]} action={()=>handleGame(8)}/>
     </div>
+    <h2>{gameStatus}</h2>
     </>
   );
 }
- 
+
 export default Board;
